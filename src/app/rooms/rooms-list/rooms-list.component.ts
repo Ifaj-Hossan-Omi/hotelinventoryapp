199cf2910ea115e39c605d35cedb6d17ee8d64ea
Md. Ifaj Hossan Omi <omiifajhossan@gmail.com>
2023-01-23 15:42:47 +0600

    4th

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomsListComponent {
-
   @Input() rooms: RoomList[] = [];
 
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom.emit(room);
+  }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -19,47 +19,68 @@ export class RoomsComponent implements OnInit {
   };
 
   roomList: RoomList[] = [];
+  selectedRoom!: RoomList;
 
   constructor() {}
 
   ngOnInit(): void {
     this.roomList = [
-        {
-          roomNumber: 1,
-          roomType: 'Deluxe',
-          amenitis: 'Air Conditioning, TV, WiFi',
-          price: 100,
-          photos: 'https://picsum.photos/200/300',
-          checkinTime: new Date('2020-01-01'),
-          checkoutTime: new Date('2020-01-02'),
-          rating: 4.5,
-        },
-        {
-          roomNumber: 2,
-          roomType: 'Super Deluxe',
-          amenitis: 'Air Conditioning, TV, WiFi',
-          price: 200,
-          photos: 'https://picsum.photos/200/300',
-          checkinTime: new Date('2020-01-01'),
-          checkoutTime: new Date('2020-01-02'),
-          rating: 5,
-        },
-        {
-          roomNumber: 3,
-          roomType: 'Luxury',
-          amenitis: 'Air Conditioning, TV, WiFi',
-          price: 300,
-          photos: 'https://picsum.photos/200/300',
-          checkinTime: new Date('2020-01-01'),
-          checkoutTime: new Date('2020-01-02'),
-          rating: 2.3,
-        },
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe',
+        amenitis: 'Air Conditioning, TV, WiFi',
+        price: 100,
+        photos: 'https://picsum.photos/200/300',
+        checkinTime: new Date('2020-01-01'),
+        checkoutTime: new Date('2020-01-02'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Super Deluxe',
+        amenitis: 'Air Conditioning, TV, WiFi',
+        price: 200,
+        photos: 'https://picsum.photos/200/300',
+        checkinTime: new Date('2020-01-01'),
+        checkoutTime: new Date('2020-01-02'),
+        rating: 5,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Luxury',
+        amenitis: 'Air Conditioning, TV, WiFi',
+        price: 300,
+        photos: 'https://picsum.photos/200/300',
+        checkinTime: new Date('2020-01-01'),
+        checkoutTime: new Date('2020-01-02'),
+        rating: 2.3,
+      },
     ];
   }
-
 
   toggle() {
     this.hideRooms = !this.hideRooms;
     // this.numberOfRooms = this.numberOfRooms === 10 ? 20 : 10;
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Luxury',
+      amenitis: 'Air Conditioning, TV, WiFi',
+      price: 300,
+      photos: 'https://picsum.photos/200/300',
+      checkinTime: new Date('2020-01-01'),
+      checkoutTime: new Date('2020-01-02'),
+      rating: 1.3,
+    };
+
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
+
   }
 }
